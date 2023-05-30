2372bf2fd21d6c498d749e80aee8418ab34ec05b
Reach <brianelliott2015@gmail.com>
2023-05-29 21:17:33 -0400

    Add all results

--- scripts.js
+++ scripts.js
@@ -5,3 +5,31 @@ function getComputerChoice() {
     let choiceNum = Math.floor(Math.random()*3);
     return choices[choiceNum];
 }
+
+function round(playerSelection, computerSelection) {
+    lowerSelect = playerSelection.toLowerCase();
+
+    if(lowerSelect==computerSelection) {
+        return "Tie game!";
+    }
+    else if(lowerSelect=="rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if(lowerSelect=="rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+    else if(lowerSelect=="paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    }
+    else if(lowerSelect=="paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats paper";
+    }
+    else if(lowerSelect=="scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+    else if(lowerSelect=="scissors" && computerSelection == "paper"){
+        return "You Win! Scissors beats Paper";
+    }
+
+    return "ERROR: Invalid selection";
+}
