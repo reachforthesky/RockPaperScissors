59e9981e04faac4ede96b156993d19341a1da4f6
Reach <brianelliott2015@gmail.com>
2023-05-31 15:38:51 -0400

    add selection highlighting and comments

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
-console.log("Let's play Rock, Paper Scissors!");
-
 let playerScore = 0;
 let compScore = 0;
 
-game();
+//add event listeners for each button
+const selections = document.querySelectorAll('.selection');
+selections.forEach(select => {
+    select.addEventListener("click" , playRound)
+});
 
+//selects a random rps choice for the computer
 function getComputerChoice() {
     let choices = ["rock" , "paper" , "scissors"];
     let choiceNum = Math.floor(Math.random()*3);
     return choices[choiceNum];
 }
 
+//determines winner based on the player's and computer's rps selection
+//returns an array where the first element is the text result, 
+//and the second is a string naming the winner or a tie
 function round(playerSelection, computerSelection) {
     lowerSelect = playerSelection.toLowerCase();
 
@@ -39,6 +45,44 @@ function round(playerSelection, computerSelection) {
     return ["ERROR: Invalid selection","none"];
 }
 
+//clears text and button border from previous selections
+function clearText(){
+    const buttonTexts = document.querySelectorAll(".button-text");
+    const buttons = document.querySelectorAll(".selection");
+    buttonTexts.forEach(text =>
+        text.textContent = ""
+    )
+    buttons.forEach(b =>
+        b.style.border = ""
+    )
+}
+
+//adds text and a border to a button indicating player selection
+function highlightPlayerSelection(pSelect) {
+    const playerSelect = document.querySelector(`#${pSelect}-text`);
+    const playerButton = document.querySelector(`#${pSelect}`);
+    playerSelect.textContent = "You selected\n";
+    playerButton.style.border = "3px inset blue";
+}
+
+
+//adds text and a border to a button indicating computer selection
+//also adds text and border if both player and computer selected
+//the same rps selection
+function highlightCompSelection(cSelect) {
+    const compSelect = document.querySelector(`#${cSelect}-text`);
+    const compButton = document.querySelector(`#${cSelect}`);
+    if(compSelect.textContent === "")
+        compSelect.textContent ="Computer selected";
+    else
+        compSelect.textContent ="Player and Computer selected";
+    if(compButton.style.border === "")
+        compButton.style.border = "3px inset red";
+    else
+        compButton.style.border = "3px inset purple";
+}
+
+//updates score text based on current score
 function updateScore(pScore, cScore) {
     const playerScoreCard = document.querySelector('#player-score');
     const compScoreCard = document.querySelector('#comp-score');
@@ -47,6 +91,9 @@ function updateScore(pScore, cScore) {
     compScoreCard.textContent = (cScore);
 }
 
+//plays a round based on player and computer choice
+//declares winner if one reaches a score of 5
+//resets scores if previous match is over
 function playRound(e){
     if(playerScore === 5 || compScore === 5){
         playerScore = 0;
@@ -54,6 +101,9 @@ function playRound(e){
     }
     let playerChoice = e.target.id;
     let compChoice = getComputerChoice();
+    clearText();
+    highlightPlayerSelection(playerChoice);
+    highlightCompSelection(compChoice);
     let info = document.querySelector('.info')
     
     let result = round(playerChoice,compChoice);
@@ -76,11 +126,4 @@ function playRound(e){
     else {
         return;
     }
-}
-
-function game() {
-    const selections = document.querySelectorAll('.selection');
-    selections.forEach(select => {
-        select.addEventListener("click" , playRound)
-    });
 }
